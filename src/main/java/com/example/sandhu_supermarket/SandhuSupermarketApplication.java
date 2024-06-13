efe187e60781bdb34f086fb67dad5d8cb9157bb2
jasdeepkaur0731 <jasdeep.Kaur@triosstudent.com>
2024-06-13 11:06:27 -0400

    a

---
+++ src/main/java/com/example/sandhu_supermarket/SandhuSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandhu_supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandhuSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandhuSupermarketApplication.class, args);
+	}
+
+}
